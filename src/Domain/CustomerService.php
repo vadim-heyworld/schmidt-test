639ff3d70b344d6567e9255253dc25177d53020f
Vadim <155449655+vadim-heyworld@users.noreply.github.com>
2024-11-29 13:48:06 +0100

    Add changes for Fix code style and naming conventions test

---
+++ src/Domain/CustomerService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Domain;
+
+class customerService {
+    private $ID;
+    private $Name;
+
+    function GetCustomer($id) {
+        return NULL;
+    }
+
+    public function set_status($status) {
+        $this -> status = $status;
+    }
+}
