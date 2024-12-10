e7be848c77ec4b8fb008fdefb0affa65e92b444c
Vadim <155449655+vadim-heyworld@users.noreply.github.com>
2024-12-10 17:20:27 +0100

    Just another check

--- MyService.php
+++ MyService.php
@@ -4,5 +4,12 @@ declare(strict_types=1);
 
 class MyService
 {
+    public function __construct(private string $name)
+    {
+    }
 
+    public function name(): string
+    {
+        return $this->name;
+    }
 }
